a2297a7638067453665784f228fcb438d41da5a4
Marc Jay <marc.jay@taskize.com>
2020-12-16 00:43:08 +0000

    Add support for loading Jira password from Rundeck's Key Storage
    - Support an optional `password_storage_path` framework or project property which specifies the path to the Jira password in Rundeck's Key Storage
    - Replace now unmaintained rcarz Jira client with Atlassian's official and maintained Jira REST Java Client Library (JRJC)
    - Update rundeck-core dependency to 3.3.4
    - Update release plugin
    
    Fixes: #19

--- src/main/java/org/rundeck/plugins/notification/JiraNotification.java
+++ src/main/java/org/rundeck/plugins/notification/JiraNotification.java
@@ -1,17 +1,23 @@
 package org.rundeck.plugins.notification;
 
 
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Comment;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.dtolabs.rundeck.core.plugins.Plugin;
+import com.dtolabs.rundeck.core.plugins.configuration.AcceptsServices;
 import com.dtolabs.rundeck.core.plugins.configuration.PropertyScope;
+import com.dtolabs.rundeck.core.storage.keys.KeyStorageTree;
 import com.dtolabs.rundeck.plugins.descriptions.PluginDescription;
 import com.dtolabs.rundeck.plugins.descriptions.PluginProperty;
 import com.dtolabs.rundeck.plugins.notification.NotificationPlugin;
+import org.rundeck.app.spi.Services;
 
-import net.rcarz.jiraclient.BasicCredentials;
-import net.rcarz.jiraclient.Issue;
-import net.rcarz.jiraclient.JiraClient;
-import net.rcarz.jiraclient.JiraException;
-
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
 
@@ -20,7 +26,7 @@ import java.util.Map;
  */
 @Plugin(service = "Notification", name = "JIRA")
 @PluginDescription(title = "JIRA Notification", description = "Append notification messages to a Jira issue.")
-public class JiraNotification implements NotificationPlugin {
+public class JiraNotification implements NotificationPlugin, AcceptsServices {
 
     @PluginProperty(name = "issue key", title = "issue key", description = "Jira issue ID")
     private String issueKey;
@@ -34,12 +40,36 @@ public class JiraNotification implements NotificationPlugin {
     @PluginProperty(name = "password", title = "password", description = "The account password", scope = PropertyScope.Project)
     private String password;
 
+    @PluginProperty(name = "password_storage_path", title = "Password Storage Path", description = "Path to the password in Key Storage", scope = PropertyScope.Project)
+    private String passwordStoragePath;
+
+    private Services services;
+
     public JiraNotification() {
 
     }
 
     @Override
+    public void setServices(Services services) {
+        this.services = services;
+    }
+
+    @Override
     public boolean postNotification(String trigger, Map executionData, Map config) {
+        if (!isBlank(passwordStoragePath)) {
+            KeyStorageTree storageTree = this.services.getService(KeyStorageTree.class);
+            if (!storageTree.hasPassword(passwordStoragePath)) {
+                throw new IllegalStateException(
+                        String.format("password_storage_path: '%s' does not point to a valid password in the Key Store", passwordStoragePath));
+            }
+            try {
+                byte[] bytes = storageTree.readPassword(passwordStoragePath);
+                password = new String(bytes, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new IllegalStateException("Exception encountered attempting to read password from Key Storage");
+            }
+        }
+
         if (null == login) {
             throw new IllegalStateException("login is required");
         }
@@ -52,18 +82,24 @@ public class JiraNotification implements NotificationPlugin {
         /**
          * Connect to JIRA using the configured credentials.
          */
-        final BasicCredentials creds = new BasicCredentials(login, password);
-        final JiraClient jira = new JiraClient(serverURL, creds);
+        URI serverUri;
+        try {
+            serverUri = new URI(serverURL);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("server URL is invalid");
+        }
+        AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+        JiraRestClient restClient = factory.createWithBasicHttpAuthentication(serverUri, login, password);
         try {
             /* Retrieve the issue from JIRA */
-            Issue issue = jira.getIssue(issueKey);
+            Issue issue = restClient.getIssueClient().getIssue(issueKey).claim();
 
             /* Add a comment to the issue */
             String message = generateMessage(trigger, executionData);
-            issue.addComment(message);
+            restClient.getIssueClient().addComment(issue.getCommentsUri(), Comment.valueOf(message));
 
-        } catch (JiraException je) {
-            je.printStackTrace(System.err);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
             return false;
         }
 
